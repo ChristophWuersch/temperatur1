da9d1feba5d44b3721c4a0ad03e1231a0e6ecba0
Christoph Würsch <christoph.wuersch@ost.ch>
2020-12-31 17:18:23 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,14 @@
+let temperatur = 0
 let tm = 0
+let count = 0
+let _4digit = grove.createDisplay(DigitalPin.P1, DigitalPin.P2)
+_4digit.set(4)
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    tm = pins.analogReadPin(AnalogPin.P3)
-    basic.showNumber(tm - tm % 5 / 5)
+    while (true) {
+        tm = pins.analogReadPin(AnalogPin.P3)
+        temperatur = tm - tm % 5 / 5
+        _4digit.show(temperatur)
+        basic.pause(100)
+    }
 })
