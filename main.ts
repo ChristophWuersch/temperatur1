995384dc034bee86b06f015252c36d2d6f3a1a7c
Christoph Würsch <christoph.wuersch@ost.ch>
2020-12-31 18:16:19 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,17 +5,16 @@ let _4digit = grove.createDisplay(DigitalPin.P1, DigitalPin.P2)
 _4digit.set(4)
 basic.showIcon(IconNames.Happy)
 makerbit.connectLcd(39)
-makerbit.setLcdBacklight(LcdBacklight.Off)
-basic.pause(1000)
 makerbit.setLcdBacklight(LcdBacklight.On)
 basic.pause(1000)
+makerbit.setLcdBacklight(LcdBacklight.Off)
 basic.forever(function () {
-    makerbit.setLcdBacklight(LcdBacklight.Off)
-    makerbit.showStringOnLcd1602("MakerBit", makerbit.position1602(LcdPosition1602.Pos1), 16)
+    makerbit.setLcdBacklight(LcdBacklight.On)
     while (true) {
         tm = pins.analogReadPin(AnalogPin.P3)
         temperatur = tm - tm % 5 / 5
         _4digit.show(temperatur)
-        basic.pause(100)
+        makerbit.showStringOnLcd1602(convertToText(temperatur), makerbit.position1602(LcdPosition1602.Pos1), 6)
+        basic.pause(1000)
     }
 })
